e9eb3141c007b7b1175f2ea2de04fc5b5116540e
tami <fko2247041@stu.o-hara.ac.jp>
2022-10-03 11:15:56 +0900

    山下用

---
+++ ShotBase.cpp
@@ -0,0 +1,50 @@
+#include "DxLib.h"
+#include "shotBase.h"
+
+namespace
+{
+	
+}
+
+ShotBase::ShotBase()
+{
+	m_handle = -1;
+	m_pos.x = 0.0f;
+	m_pos.y = 0.0f;
+
+	m_vec.x = 0.0f;
+	m_vec.y = 0.0f;
+
+	m_isExist = false;
+}
+
+ShotBase::~ShotBase()
+{
+
+}
+
+void Shot::start(Vec2 pos)
+{
+	m_isExist = true;
+	m_pos = pos;
+
+	m_vec.x = kShotSpeed;
+	m_vec.y = 0.0f;
+}
+
+void Shot::update()
+{
+	if (!m_isExist) return;
+	m_pos += m_vec;
+
+	if (m_pos.x > Game::kScreenWidth)
+	{
+		m_isExist = false;
+	}
+}
+// •\Ž¦
+void Shot::draw()
+{
+	if (!m_isExist) return;
+	DrawGraphF(m_pos.x, m_pos.y, m_handle, true);
+}
